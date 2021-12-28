17265f892645fd3b2874afb6847778c9f277ab5f
mlalisse <matthias.lalisse@gmail.com>
2021-12-28 17:24:40 -0500

    Update utils.py

--- TweetDatasets/utils.py
+++ TweetDatasets/utils.py
@@ -18,7 +18,6 @@ def atoi(text):
 def natural_keys(text):
     '''
     alist.sort(key=natural_keys) sorts in human order
-    (See Toothy's implementation in the comments)
     '''
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
